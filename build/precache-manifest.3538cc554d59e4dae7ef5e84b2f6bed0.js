e1c5dd5fb450cfd025ce9c95c43bbde5d333cd0a
Clinton Joy <96880045+Cejay101@users.noreply.github.com>
2022-09-30 23:40:12 +0100

    Add files via upload

---
+++ build/precache-manifest.3538cc554d59e4dae7ef5e84b2f6bed0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d95ff19db57f0baaa68",
+    "url": "/static/css/main.c8c3c847.chunk.css"
+  },
+  {
+    "revision": "4d95ff19db57f0baaa68",
+    "url": "/static/js/main.4d95ff19.chunk.js"
+  },
+  {
+    "revision": "4d2681f1e613f1601114",
+    "url": "/static/js/1.4d2681f1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2041e38afad14be81031971085823c8",
+    "url": "/static/media/Webp 2.b2041e38.png"
+  },
+  {
+    "revision": "85d687c3fa19ef70c0761ffd22145372",
+    "url": "/static/media/hands.85d687c3.png"
+  },
+  {
+    "revision": "24de31de94054501540a1c4cb81ca0bf",
+    "url": "/static/media/Kevin.24de31de.png"
+  },
+  {
+    "revision": "91d3f2b100e02b3b45c10763fb45b816",
+    "url": "/static/media/Nancy.91d3f2b1.png"
+  },
+  {
+    "revision": "b970bdfa26c0a499bda4cd0de1ddd6d2",
+    "url": "/static/media/Jordan.b970bdfa.png"
+  },
+  {
+    "revision": "6dca4eb78f70ce44fdbd870eebeea594",
+    "url": "/static/media/Asya.6dca4eb7.png"
+  },
+  {
+    "revision": "c2f26eb4fb3ed478854f4b9e8127a5bf",
+    "url": "/static/media/maps.c2f26eb4.png"
+  },
+  {
+    "revision": "e4bc34d62f1411ed9bfb629b4e41092b",
+    "url": "/index.html"
+  }
+];
